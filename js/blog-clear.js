76514494a0c5c414c09d21f37c58c0da7a711a50
Mohamad Arouni <mohamadarouni5@gmail.com>
2025-08-12 19:22:41 +0100

    no need for that and removed a link from the mobile menu

--- js/blog-clear.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("clearBtn").addEventListener("click", function() {
-    if (confirm("Are you sure you want to clear the fields?")) {
-        document.getElementById("title").value = "";
-        document.getElementById("content").value = "";
-    }
-});
